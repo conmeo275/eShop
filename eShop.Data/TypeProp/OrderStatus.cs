288b1b6d66e238c4147ccf7f304ac18ac1d69319
conmeo275 <danganhhaivl@gmail.com>
2020-10-28 09:33:38 +0700

    Create Class Model

---
+++ eShop.Data/TypeProp/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.TypeProp
+{
+    public enum OrderStatus
+    {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
+    }
+}
